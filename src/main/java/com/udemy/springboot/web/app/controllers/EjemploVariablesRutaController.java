3b1d664a2c0014e1e375237009d09f985929e860
JvRdgz <javier.rodriguezjr97@gmail.com>
2022-10-29 21:36:10 +0200

    Video 24

--- src/main/java/com/udemy/springboot/web/app/controllers/EjemploVariablesRutaController.java
+++ src/main/java/com/udemy/springboot/web/app/controllers/EjemploVariablesRutaController.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/variables")
 public class EjemploVariablesRutaController {
 	
+	@GetMapping("/")
+	public String index (Model model) {
+		model.addAttribute("titulo", "Recibir parámetros de la ruta (@PathVariable)");
+		return "variables/index";
+	}
+	
 	/**
 	 * Metodo PathVariable.
 	 * El nombre del parametro tiene que ser igual que el nombre indicado entre {} del metodo Handler (GetMapping)
@@ -18,9 +24,15 @@ public class EjemploVariablesRutaController {
 	 */
 	@GetMapping("/string/{texto}")
 	public String variables(@PathVariable String texto, Model model) {
-		model.addAttribute("titulo", "Recibir parámetros de la ruta (@PathVariable");
+		model.addAttribute("titulo", "Recibir parámetros de la ruta (@PathVariable)");
 		model.addAttribute("resultado", "El texto enviado en la ruta es: " + texto);
 		return "variables/ver";
 	}
 
+	@GetMapping("/string/{texto}/{numero}")
+	public String variables(@PathVariable String texto, @PathVariable Integer numero, Model model) {
+		model.addAttribute("titulo", "Recibir parámetros de la ruta (@PathVariable)");
+		model.addAttribute("resultado", "El texto enviado en la ruta es: " + texto + " y el numero en el path es: " + numero);
+		return "variables/ver";
+	}
 }
